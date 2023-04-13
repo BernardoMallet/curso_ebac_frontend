4338368ebb4cae74f8859eda9da7cf9414145be5
Bernardo Mallet <bernardomallet28@gmail.com>
2023-04-13 13:20:51 -0300

    finalizando exercicio do modulo de typescript

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,12 @@
+const calcArea = (base: number, altura: number) => {
+    return base * altura;
+}
+
+console.log(calcArea(10, 20));
+
+
+const saudacao = (nome: string) => {
+    return 'Olá, ' + nome + '!';
+}
+
+console.log(saudacao('EBAC'));
